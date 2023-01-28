d0d0b5738955201f4c6674922f73aa2d43956f54
Yun Ji <this.jiyun@gmail.com>
2023-01-28 11:27:49 +0100

    formatting

---
+++ nextjs-blog/pages/posts.js
@@ -0,0 +1,58 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      
+
+      <section className={utilStyles.headingMd}>
+        <p className='text-slate-600'>Hi I am Yun</p>
+        <p>
+          I built web applications with Python/Django. I am studying front-end development with MigraCode Barcelona.I give private English lessons and write articles about climbing. Before that, I worked as a Senior Tech Reporter in Beijing.I interpreted for Elon Musk,and interviwed the founder of TikTok when Tiktok had not been created yet.
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                {title}
+              </Link>
+
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+
+          ))}
+
+        </ul>
+
+      </section>
+
+    </Layout>
+  )
+}
