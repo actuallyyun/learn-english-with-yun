a5ef32650329d61ecc4b08e63f6637c8516323ec
Yun Ji <this.jiyun@gmail.com>
2023-02-02 14:19:04 +0100

    feat:add layout

--- nextjs-blog/pages/faq.js
+++ nextjs-blog/pages/faq.js
@@ -1,5 +1,30 @@
-const FAQ = () => {
-  return <div>wip</div>;
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+const getAllFaqs = () => {
+  const faq = [
+    { q: "a test question 1", a: "a test answer 1" },
+    {
+      q: "test question 2",
+      a: "test answer 2",
+    },
+  ];
+  return { params: { faqs: faq } };
+};
+
+const FAQ = (faqs) => {
+  console.log(faqs);
+  return (
+    <Layout>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <div className="grid justify-items-center bg-gradient-to-t from-slate-100 font-sans">
+        <div className="w-11/12 md:w-3/4">wip</div>
+      </div>
+    </Layout>
+  );
 };
 
 export default FAQ;
