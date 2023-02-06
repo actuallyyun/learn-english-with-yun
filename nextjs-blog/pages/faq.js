dcaa4ec101dee4f83e95e95f22f6ec8a3f22b891
Yun Ji <this.jiyun@gmail.com>
2023-02-06 13:28:53 +0100

    feat:parse faq.md to faq page

--- nextjs-blog/pages/faq.js
+++ nextjs-blog/pages/faq.js
@@ -1,30 +1,31 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import getSortedMarkdownData from "../lib/getMarkDownData";
 
-const getAllFaqs = () => {
-  const faq = [
-    { q: "a test question 1", a: "a test answer 1" },
-    {
-      q: "test question 2",
-      a: "test answer 2",
-    },
-  ];
-  return { params: { faqs: faq } };
-};
+//test if data passed  to the component correctly
 
-const FAQ = (faqs) => {
-  console.log(faqs);
+export default function FAQ(faqData) {
+  console.log(faqData);
   return (
     <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <div className="grid justify-items-center bg-gradient-to-t from-slate-100 font-sans">
-        <div className="w-11/12 md:w-3/4">wip</div>
+        <div className="w-11/12 md:w-3/4">
+          <h3>{faqData.faqData.title}</h3>
+          <div
+            dangerouslySetInnerHTML={{ __html: faqData.faqData.contentHtml }}
+          />
+        </div>
       </div>
     </Layout>
   );
-};
+}
 
-export default FAQ;
+export async function getStaticProps({ params }) {
+  const faqPath = "/public/infos/faq.md";
+  const faqData = await getSortedMarkdownData(faqPath);
+  return { props: { faqData } };
+}
